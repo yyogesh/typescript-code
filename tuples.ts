f8fd6a6e9d4ee2e6dd5097a427247383de862d99
yyogesh <yogeshggn1@gmail.com>
2022-03-30 13:40:52 +0530

    typescript tuples

---
+++ tuples.ts
@@ -0,0 +1,28 @@
+type ThreeDCoordinate = [x: number, y: number, z: number];
+
+function add3DCoordinate(c1: ThreeDCoordinate, c2: ThreeDCoordinate): ThreeDCoordinate {
+    return [
+        c1[0] + c2[0],
+        c1[1] + c2[1],
+        c1[2] + c2[2],
+    ]
+}
+
+console.log(add3DCoordinate([1, 2, 3], [10, 20, 30]));
+
+
+function simpleStringState(initial: string): [() => string, (v: string) => void] {
+    let str: string = initial;
+    return [
+        () => str,
+        (v: string) => {
+            str = v;
+        }
+    ]
+}
+
+const [str1Getter, str1Setting] = simpleStringState('Hello');
+console.log(str1Getter());
+str1Setting('test');
+console.log(str1Getter());
+
