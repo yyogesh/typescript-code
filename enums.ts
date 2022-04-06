a61c4e5fb09224944085b2487cd2e4ffcd32c798
yyogesh <yogeshggn1@gmail.com>
2022-04-06 09:42:07 +0530

    enum and literal

---
+++ enums.ts
@@ -0,0 +1,29 @@
+const beforeLoad = 'beforeLoad';
+const loading = 'loading';
+const loaded = 'loaded';
+
+const isLoading = (state: string) => state === loading;
+
+console.log(isLoading("dog"));
+
+
+enum LoadingState {
+    beforeLoad = 'beforeLoad',
+    loading = 'loading',
+    loaded = 'loaded'
+}
+
+const isLoading1 = (state: LoadingState) => state === LoadingState.loading;
+
+console.log(isLoading1(LoadingState.beforeLoad));
+
+const englishLoadingStates = {
+    [LoadingState.beforeLoad]: "Before Load"
+}
+
+function sendEvent(name: 'checkout', data: { cartCount: number }): void;
+function sendEvent(name: 'abc', data: unknown): void {
+    console.log(`${name} : ${JSON.stringify(data)}`)
+}
+
+sendEvent('checkout', { cartCount: 1 })
